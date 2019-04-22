c747fae96128ce3fe94d5e62e31cdaee8e2bd6e1
stonegray <stone@stonegray.ca>
2019-04-22 12:56:15 -0400

    update install script

--- install.sh
+++ install.sh
@@ -6,17 +6,23 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" || exit
 # Files to be linked
 array=( 
 	".sshrc" 
+	".bashrc"
+	".bash_profile"
+	".zshrc"
+	".profile"
 	".vimrc" 
 	".sshrc.d"
-       	".tmate.conf" 
+    ".tmate.conf" 
+	".tmate2"
 	".tmux.conf.local" 
 )
+
+# Append file name. Probably a better way of doing this.
 array=( "${array[@]/#/$(pwd)/}" )
 
 # Link to $HOME
 echo "> linking ${array[@]} into ~"
-
-
-# Link if we can, otherwise show an error.
 ln -s ${array[@]} $HOME
 
+
+ln -s ./.config/kitty/kitty.conf ~/.config/kitty/
